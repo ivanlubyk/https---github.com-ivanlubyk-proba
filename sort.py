c74aa81b40bce9df690d69ba84edc89534252e19
v.dunkan <v.dunkan@gmail.com>
2023-03-05 16:58:14 +0200

    by Skiff

--- sort.py
+++ sort.py
@@ -17,13 +17,22 @@ category = {
 #     return namesif s.suffix in audios:
 # #             os.path.join(s, new_audios
 
+def move_file(target_folder:Path, file: Path):
+    extension = file.suffix.lower()
+    for cat, extensions in category.items():
+        if extension in extensions:
+            return file.replace(target_folder / cat / file.name)
+    return file.replace(target_folder / file.name)
+
 def sort(path : Path):
-    dir_list = []
-    file_list = []
-    list_dir(path, file_list, dir_list)
-    cat_dirs = create_cat_dirs(path)
-    for file in file_list:
-        if file.suffix in category["images"]:
+    # dir_list = []
+    # file_list = []
+    # list_dir(path, file_list, dir_list)
+    # cat_dirs = create_cat_dirs(path)
+    # for file in file_list:
+    #     if file.suffix in category["images"]:
+    for file in path.glob('**/*.*'):
+        move_file(path, file)
             
 
     # for d in dir_list:
@@ -32,24 +41,28 @@ def sort(path : Path):
     #     else:
     #         names.append(i)
 
-def list_dir(path:Path, files: [], dirs: []):
-    for p in path.iterdir():
-        if p.is_dir() and p.name not in category.keys():
-            dirs.append(p)
-        elif p.is_file():
-            files.append(p)
-
-
+# def list_dir(path:Path, files: [], dirs: []):
+#     for p in path.iterdir():
+#         if p.is_dir() and p.name not in category.keys():
+#             dirs.append(p)
+#         elif p.is_file():
+#             files.append(p)
 
 
 def create_cat_dirs(path:Path):
     dirs = {}
     for cat in category.keys():
-        cat_dir = Path(path, cat)
+        # cat_dir = Path(path, cat)
+        cat_dir = path / cat
         if not cat_dir.exists():
             cat_dir.mkdir()
             dirs[cat] = cat_dir
     return dirs
 
 
-sort(Path('D:\DZ'))
+# sort(Path('D:\DZ'))
+
+if __name__ == '__main__':
+    path = Path(r'D:\DZ')
+    create_cat_dirs(path)
+    sort(path)
