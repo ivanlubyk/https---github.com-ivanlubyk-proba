9e087d9102f84bbf6b0931d978e0c105630d64ff
v.dunkan <v.dunkan@gmail.com>
2023-03-08 23:08:34 +0200

    By Skiff

--- sort 1.py
+++ sort 1.py
@@ -1,13 +1,15 @@
 import shutil
 from pathlib import Path
 import re
+import sys
 
 category = {
     "images": ['.jpeg', '.png', '.jpg', '.svg'],
     "audio": ['.mp3', '.ogg', '.wav', '.amr'],
     "video": ['.avi', '.mp4', '.mov', '.mkv'],
     "documents": ['.doc', '.docx', '.txt', '.pdf', '.xlsx', '.pptx'],
-    "archives": ['.zip', '.gz', '.tar']
+    "archives": ['.zip', '.gz', '.tar'],
+    "unknown": []
 }
 
 
@@ -21,20 +23,25 @@ def create_cat_dirs(path: Path):
 CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
 TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
                "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "ya", "je", "i", "ji", "g")
+BAD_SYMBOLS = (" ", "@", "!", "-", "+")
+
 TRANS = {}
 
 for c, l in zip(list(CYRILLIC_SYMBOLS), TRANSLATION):
     TRANS[ord(c)] = l
     TRANS[ord(c.upper())] = l.upper()
 
+for symbol in BAD_SYMBOLS:
+    TRANS[ord(symbol)] = "_"
+
 
 def normalize_name(name: str):
     # TODO Зробити
     tranlation = name.translate(TRANS)
-    for s in tranlation:
-        done_rep = s.replace(' ', '_').replace('@', '_').replace('!', '_').replace('-', '_')\
-            .replace('+', '_')
-    return done_rep
+    # for s in tranlation:
+    #     done_rep = s.replace(' ', '_').replace('@', '_').replace('!', '_').replace('-', '_')\
+    #         .replace('+', '_')
+    return name.translate(TRANS)
 
 
 def unpack_arch(arch: Path, target_dir: Path):
@@ -46,25 +53,43 @@ def unpack_arch(arch: Path, target_dir: Path):
 
 def move_file(target_folder: Path, file: Path):
     extension = file.suffix.lower()
+    is_moved = False
     for cat, extensions in category.items():
+        new_place: Path = None
         if extension in extensions:
-            if cat == "archives":
-                return unpack_arch(file, target_folder / cat)
-            else:
-                to_file = target_folder / cat / normalize_name(file.name)
-                print(f"moving file: {file}, to {to_file}")
-                return file.replace(to_file)
+            to_file = target_folder / cat / normalize_name(file.name)
+            print(f"moving file: {file}, to {to_file}")
+            new_place = file.replace(to_file)
+            is_moved = True
+        if cat == "archives" and new_place:
+            unpack_arch(new_place, target_folder / cat/ new_place.stem)
+    if not is_moved:
+        file.replace(target_folder / 'unknown' / normalize_name(file.name))
+
+        
 
 
 def sort(path: Path):
-    for file in path.rglob("*"):
+    for file in path.rglob("**/*.*"):
         move_file(path, file)
+        
+def main():
+    try:
+        path = Path(r"d:\testfolder") #Path(sys.argv[1])
+    except IndexError:
+        print("Sorry, must be parameter - path to folder")
+        return None
+
+    if not path.exists():
+        print(f"The folder - {path} dos't exist")
+        return None
+    
+    create_cat_dirs(path)
+    sort(path)
 
 
 if __name__ == "__main__":
-    path = Path("D:\DZ")
-    create_cat_dirs(path)
-    sort(path)
+    main()
 
 
 
