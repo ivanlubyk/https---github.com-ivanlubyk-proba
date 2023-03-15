6175c332930b1e5a80ddc0ddddaeac6ac8c02fbb
Іван Лубик <126516121+ivanlubyk@users.noreply.github.com>
2023-03-16 01:33:24 +0200

    Update data_birs.py

--- data_birs.py
+++ data_birs.py
@@ -21,3 +21,11 @@ def get_birthdays_per_week(users):
         day_name = day.strftime("%A")
         if day_name in current_birthdays:
             print(f"{day_name}: {', '.join(current_birthdays[day_name])}")
+    
+if __name__ == "__main__":
+get_birthdays_per_week([{'name': 'Bill1', 'birthday': datetime(1999, 3, 17)},
+                            {'name': 'Bill2', 'birthday': datetime(1985, 3, 19)},
+                            {'name': 'Bill3', 'birthday': datetime(1990, 3, 20)},
+                            {'name': 'Bill4', 'birthday': datetime(2001, 3, 21)},
+                            {'name': 'Bill5', 'birthday': datetime(1995, 3, 22)},
+                            {'name': 'Bill7', 'birthday': datetime(1985, 4, 19)}])
