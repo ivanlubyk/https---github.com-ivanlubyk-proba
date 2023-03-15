b2832a43c498467d9b50e80c9b7ac0226d65e45d
Іван Лубик <126516121+ivanlubyk@users.noreply.github.com>
2023-03-16 01:30:55 +0200

    Update data_birs.py

--- data_birs.py
+++ data_birs.py
@@ -1,7 +1,6 @@
 
 from collections import defaultdict
-from datetime import datetime
-from pprint import pprint
+from datetime import datetime, timedelta
 
 
 def get_birthdays_per_week(users):
@@ -21,13 +20,4 @@ def get_birthdays_per_week(users):
         day = start_of_week + timedelta(days=i)
         day_name = day.strftime("%A")
         if day_name in current_birthdays:
-            print(f"{day_name}: {', '.join(current_birthdays[day_name])}") 
-
-
-if __name__ == "__main__":
-    get_birthdays_per_week([{'name': 'Bill1', 'birthday': datetime(1999, 3, 17)},
-                            {'name': 'Bill2', 'birthday': datetime(1985, 3, 19)},
-                            {'name': 'Bill3', 'birthday': datetime(1990, 3, 20)},
-                            {'name': 'Bill4', 'birthday': datetime(2001, 3, 21)},
-                            {'name': 'Bill5', 'birthday': datetime(1995, 3, 22)},
-                            {'name': 'Bill7', 'birthday': datetime(1985, 4, 19)}])
+            print(f"{day_name}: {', '.join(current_birthdays[day_name])}")
